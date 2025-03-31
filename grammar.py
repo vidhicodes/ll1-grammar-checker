c95d931e0610dee0a05450162d018dcc6e6d79b4
vidhicodes <vidhidhakate1084@gmail.com>
2025-03-31 19:42:21 +0530

    Create grammar.py

---
+++ grammar.py
@@ -0,0 +1,7 @@
+class GrammarRules:
+    def __init__(self):
+        self.articles = {"the", "a", "an"}
+        self.nouns = {"boy", "girl", "dog", "cat", "man", "apple", "ball"}
+        self.verbs = {"eats", "plays", "runs", "sees", "likes"}
+        self.objects = {"apple", "ball", "dog", "cat", "man", "girl", "boy"}
+
