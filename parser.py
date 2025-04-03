a47279eb2256053e9761349ef70528b875cff29f
vidhicodes <vidhidhakate1084@gmail.com>
2025-04-03 23:47:51 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,37 +1,32 @@
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk import pos_tag
-from first_follow import grammar_rules
+from agreement import validate_sentence
+
+def simple_tokenize(sentence):
+    return sentence.split()
 
 def parse_sentence(sentence):
-    if not sentence:
+    sentence = sentence.strip()
+    if not validate_sentence(sentence):
         return False, []
 
-    sentence = sentence[:-1].lower() + " $"
-    words = word_tokenize(sentence)
-    tagged_words = pos_tag(words)
+    words = simple_tokenize(sentence)
 
     stack = ["$", "S"]
     pointer = 0
     parsing_steps = []
-    step = 1
 
     while stack:
-        top = stack[-1]
+        top = stack.pop()
         remaining_input = " ".join(words[pointer:])
 
-        if top == words[pointer]:
+        if pointer < len(words) and top == words[pointer]:
             action = f"Matched '{top}', consuming input"
-            stack.pop()
             pointer += 1
-        elif top in grammar_rules:
-            stack.pop()
-            stack.extend(reversed(grammar_rules[top][0].split()))
-            action = f"Expanding {top} → {grammar_rules[top][0]}"
+        elif top in ["NP", "VP", "N", "V"]:
+            stack.extend(reversed(["N", "V"]))
+            action = f"Expanding {top} → N V"
         else:
-            return False, []
+            return False, parsing_steps
 
-        parsing_steps.append((step, " ".join(stack), remaining_input, action))
-        step += 1
+        parsing_steps.append((top, remaining_input, action))
 
     return pointer == len(words), parsing_steps
